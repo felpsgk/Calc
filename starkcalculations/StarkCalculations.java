a44b7dccc1d8f0510299ccd3529e3ae60fa2ef41
felipe <contatofemachado@gmail.com>
2018-06-02 17:56:50 -0300

    Primeiro commit
    Projeto simples de calculadora

---
+++ starkcalculations/StarkCalculations.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package starkcalculations;
+
+/**
+ *
+ * @author Felipe
+ */
+public class StarkCalculations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //NOVA
+        CalcTela calculadora = new CalcTela();
+        calculadora.setVisible(true);
+    }
+    
+}
